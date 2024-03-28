fb41d913848e8920cf0741aa308665c8f2d2b653
Alan Ibarra <alanjibarradev@gmail.com>
2024-03-28 12:39:45 -0700

    write file to public

--- delays.js
+++ delays.js
@@ -1,27 +1,3 @@
-// const spinnerChars = ["|", "/", "-", "\\"];
-// let spinnerIndex = 0;
-// let spinnerInterval;
-
-// // Starts a loading animation in terminal.
-// const startSpinner = () => {
-//   if (!spinnerInterval) {
-//     spinnerInterval = setInterval(() => {
-//       process.stdout.clearLine(0);
-//       process.stdout.cursorTo(0);
-//       process.stdout.write(spinnerChars[spinnerIndex]);
-//       spinnerIndex = (spinnerIndex + 1) % spinnerChars.length;
-//     }, 250);
-//   }
-// };
-
-// // Stops loading animation in terminal.
-// const stopSpinner = () => {
-//   clearInterval(spinnerInterval);
-//   spinnerInterval = null;
-//   process.stdout.clearLine(0);
-//   process.stdout.cursorTo(0);
-// };
-
 // Makes fetch request with a small delay of 1 to 3 seconds.
 const smallFetchDelay = async (url) => {
   const delay = Math.floor(Math.random() * 3) + 1;
